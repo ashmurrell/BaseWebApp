2d1987fe240cd820c796e42a3ebe59553d9cd952
Ash M <ash.e.murrell@gmail.com>
2020-11-29 23:06:23 +1100

    updated jaascript for weather collection api

--- project/js/main.js
+++ project/js/main.js
@@ -1,11 +1,24 @@
 
-function myFunction() {
+//function myFunction() {
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
 
-  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
+  ///$("#demo").html("NEWWW PARAGRAPH #javascript #fire");
 
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+ /// $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+//}
+
+$(document).ready(function(){
+getweather();
+})
+
+function getWeather() {
+var url = "https://api.openweathermap.org/data/2.5/weather?q={Sydney}&appid="+apikey;
+  $.ajax(url,{success:function(data){
+    console.log(data);}
+}
 }
 
+// $(".city").text(data.name)
+// $(".temp").text(data.main.temp)
