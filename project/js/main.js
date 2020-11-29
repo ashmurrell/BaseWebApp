2d8ddbcbe05521ca6f41ecdcd81ca772865b69ef
Ash M <ash.e.murrell@gmail.com>
2020-11-29 23:26:42 +1100

    fixed APIkey variable

--- project/js/main.js
+++ project/js/main.js
@@ -14,7 +14,7 @@ getWeather();
 })
 
 function getWeather() {
-var url = "https://api.openweathermap.org/data/2.5/weather?q={Sydney}&appid="+apikey;
+var url = "https://api.openweathermap.org/data/2.5/weather?q={Sydney}&appid="+apiKey;
 
   $.ajax(url,{success: function(data){
     console.log(data);
