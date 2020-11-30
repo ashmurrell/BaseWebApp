8f8912f32456fbe6dce0bcd1aebdeebf3d81f86e
Ash M <ash.e.murrell@gmail.com>
2020-11-30 16:45:14 +1100

    clear search box

--- project/js/main.js
+++ project/js/main.js
@@ -16,11 +16,17 @@ getWeather();
 function getWeather(searchQuery) {
 var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=metric&appid="+apiKey;
 
+$(".city").text("")
+$(".temp").text("")
+
   $.ajax(url,{success: function(data){
     console.log(data);
     $(".city").text(data.name)
     $(".temp").text(data.main.temp)
-  }})
+  },error: function(error){
+    $(".error-message").text("An error occured")
+  }
+  })
 }
 
 function searchWeather() {
