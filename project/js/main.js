9d03a54eadd1ac665b83084bc22a7f82c4b388d7
Ash M <ash.e.murrell@gmail.com>
2020-11-30 16:07:32 +1100

    metric

--- project/js/main.js
+++ project/js/main.js
@@ -14,7 +14,7 @@ getWeather();
 })
 
 function getWeather() {
-var url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid="+apiKey;
+var url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid="+apiKey;
 
   $.ajax(url,{success: function(data){
     console.log(data);
