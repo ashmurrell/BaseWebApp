9d3f4d7ca4099ac339e7508a4e182693abce94e2
Ash M <ash.e.murrell@gmail.com>
2020-11-30 16:22:39 +1100

    added search functionality

--- project/js/main.js
+++ project/js/main.js
@@ -14,12 +14,17 @@ getWeather();
 })
 
 function getWeather() {
-var url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid="+apiKey;
+var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=metric&appid="+apiKey;
 
   $.ajax(url,{success: function(data){
     console.log(data);
     $(".city").text(data.name)
     $(".temp").text(data.main.temp)
   }})
+}
+
+function searchWeather() {
+  var searchQuery=$(".search").val();
+  getWeather(searchQuery);
 }
 
