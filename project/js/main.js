966da9a5491d4d4792777f45b4522a646ce5fba0
Ash M <ash.e.murrell@gmail.com>
2020-12-01 12:15:23 +1100

    new pages

--- project/js/main.js
+++ project/js/main.js
@@ -1,34 +1,80 @@
+function handleFormSubmit(){
 
-//function myFunction() {
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // jQuery can do a lot of crazy stuff so make sure to google around to find out more
+var contactData =
 
-  ///$("#demo").html("NEWWW PARAGRAPH #javascript #fire");
+    { firstname: $("#first-name").val(),
+    lastname: $("#last-name").val(),
+    email: $("#email").val()
+    };
+    console.log(firstname);
+    console.log(lastname);
+    console.log(email);
 
-  // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
- /// $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
-//}
-
-$(document).ready(function(){
-getWeather();
-})
-
-function getWeather(searchQuery) {
-var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=metric&appid="+apiKey;
-
-  $(".city").text("")
-  $(".temp").text("")
-
-  $.ajax(url,{success: function(data){
-    $(".city").text(data.name);
-    $(".temp").text(data.main.temp);
-  }, error: function(error){
-    $(".error-message").text("An error occured");
-  }})
+  JSON.stringify(contactData);
+  
+  submitContact(firstName,lastName,email);
 }
 
-function searchWeather() {
-  var searchQuery= $(".search").val();
-  getWeather(searchQuery);
-}
 
+function submitContact(contactData){
+  // Create the new request 
+  var xhr = new XMLHttpRequest();
+  var url = 'https://api.hsforms.com/submissions/v3/integration/submit/8915946/7c640b86-0464-4120-80c5-d2752203bdea'
+  
+  // Example request JSON:
+  var data = {
+    //"submittedAt": "1517927174000",
+    "fields": [
+      {
+        "name": "email",
+        "value": email
+      },
+      {
+        "name": "firstname",
+        "value": firstname
+      },
+      {
+        "name": "lastname",
+        "value": lastname
+      }
+    ],
+    "context": {
+      //"hutk": ':hutk', // include this parameter and set it to the hubspotutk cookie value to enable cookie tracking on your submission
+      "pageUri": "www.example.com/page",
+      "pageName": "Example page"
+    },
+    "legalConsentOptions":{ // Include this object when GDPR options are enabled
+      "consent":{
+        "consentToProcess":true,
+        "text":"I agree to allow Example Company to store and process my personal data.",
+        "communications":[
+          {
+            "value":true,
+            "subscriptionTypeId":999,
+            "text":"I agree to receive marketing communications from Example Company."
+          }
+        ]
+      }
+    }
+  }
+
+  var final_data = JSON.stringify(data)
+
+  xhr.open('POST', url);
+  // Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
+  xhr.setRequestHeader('Content-Type', 'application/json');
+
+  xhr.onreadystatechange = function() {
+      if(xhr.readyState == 4 && xhr.status == 200) { 
+          alert(xhr.responseText); // Returns a 200 response if the submission is successful.
+      } else if (xhr.readyState == 4 && xhr.status == 400){ 
+          alert(xhr.responseText); // Returns a 400 error the submission is rejected.          
+      } else if (xhr.readyState == 4 && xhr.status == 403){ 
+          alert(xhr.responseText); // Returns a 403 error if the portal isn't allowed to post submissions.           
+      } else if (xhr.readyState == 4 && xhr.status == 404){ 
+          alert(xhr.responseText); //Returns a 404 error if the formGuid isn't found     
+      }
+     }
+  // Sends the request 
+  xhr.send(final_data)
+}
