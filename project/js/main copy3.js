be99850fd054e9a64399968bafa6d93ac9d8f70a
Ash M <ash.e.murrell@gmail.com>
2020-12-01 15:18:30 +1100

    fixed API submit form!

---
+++ project/js/main copy3.js
@@ -0,0 +1,34 @@
+
+function submitContact(){
+
+var data = {
+  fields: [
+    {
+      name: "email",
+      value: "testing@testing12345.com"
+    },
+    {
+      name: "firstname",
+      value: "Test"
+    },
+    {
+      name: "lastname",
+      value: "Tester"
+    },
+    {
+      name: "subject",
+      value: "Testing Subject"
+    }
+  ]
+}
+
+fetch("https://api.hsforms.com/submissions/v3/integration/submit/8915946/7c640b86-0464-4120-80c5-d2752203bdea", {
+  method: "POST",
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(data)
+}).then(res => {
+  console.log("Request complete! response:", res);
+});
+}
--- project/js/main.js
+++ project/js/main.js
@@ -1,80 +1,51 @@
-function handleFormSubmit(){
+//$(document).ready(function(){
+//  $('#contactform').submit(function (e, data) {
+   //   console.clear();
+  //    e.preventDefault();
+ //     submitContact();
+//  });
+//});
 
-var contactData =
-
-    { firstName: $("#first-name").val(),
-    lastName: $("#last-name").val(),
-    Email: $("#email").val()
-    };
-    console.log(firstName);
-    console.log(lastName);
-    console.log(Email);
-
-  JSON.stringify(contactData);
-
-  submitContact(firstName,lastName,Email);
-}
+function submitContact() {
 
 
-function submitContact(contactData){
-  // Create the new request 
-  var xhr = new XMLHttpRequest();
-  var url = 'https://api.hsforms.com/submissions/v3/integration/submit/8915946/7c640b86-0464-4120-80c5-d2752203bdea'
-  
-  // Example request JSON:
-  var data = {
-    //"submittedAt": "1517927174000",
-    "fields": [
-      {
-        "name": "email",
-        "value": Email
-      },
-      {
-        "name": "firstname",
-        "value": firstName
-      },
-      {
-        "name": "lastname",
-        "value": lastName
-      }
-    ],
-    "context": {
-      //"hutk": ':hutk', // include this parameter and set it to the hubspotutk cookie value to enable cookie tracking on your submission
-      "pageUri": "www.example.com/page",
-      "pageName": "Example page"
-    },
-    "legalConsentOptions":{ // Include this object when GDPR options are enabled
-      "consent":{
-        "consentToProcess":true,
-        "text":"I agree to allow Example Company to store and process my personal data.",
-        "communications":[
-          {
-            "value":true,
-            "subscriptionTypeId":999,
-            "text":"I agree to receive marketing communications from Example Company."
-          }
-        ]
-      }
-    }
-  }
-
-  var final_data = JSON.stringify(data)
-
-  xhr.open('POST', url);
-  // Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
-  xhr.setRequestHeader('Content-Type', 'application/json');
-
-  xhr.onreadystatechange = function() {
-      if(xhr.readyState == 4 && xhr.status == 200) { 
-          alert(xhr.responseText); // Returns a 200 response if the submission is successful.
-      } else if (xhr.readyState == 4 && xhr.status == 400){ 
-          alert(xhr.responseText); // Returns a 400 error the submission is rejected.          
-      } else if (xhr.readyState == 4 && xhr.status == 403){ 
-          alert(xhr.responseText); // Returns a 403 error if the portal isn't allowed to post submissions.           
-      } else if (xhr.readyState == 4 && xhr.status == 404){ 
-          alert(xhr.responseText); //Returns a 404 error if the formGuid isn't found     
-      }
+ var data = {
+   'fields': [
+     {'name': 'firstname',
+     'value': $("#first-name").val()},
+     //'value': "John"},
+     {'name': 'lastname',
+     'value': $("#last-name").val()},
+     //'value': "Tester"},
+     {'name': 'email',
+     'value': $("#email").val()},
+     //'value': "ash.e.murrell@gmail.com"}
+   ],
+   "legalConsentOptions": {
+     "consent": { // Include this object when GDPR options are enabled
+       "consentToProcess": true,
+       "text": "I agree to allow Able to store and process my personal data.",
+       // "communications": [
+       //   {
+       //     "value": true,
+       //     "text": "I agree to receive marketing communications from Example Company."
+       //   }
+       // ]
      }
-  // Sends the request 
-  xhr.send(final_data)
+   },
+   'skipValidation': true,
+
+ }; 
+
+ data = JSON.stringify(data)
+
+ $.ajaxSetup({
+  contentType: 'application/json'
+});
+
+$.post('https://api.hsforms.com/submissions/v3/integration/submit/8915946/7c640b86-0464-4120-80c5-d2752203bdea', data)
+ .then(function (response) {
+   console.log(response);
+ }) 
+
 }
